7a5ab24b40f7f6e9b7128c2d4acebd3f0663a80c
LeeJ1nHyeong <wlsgud4084@naver.com>
2024-07-23 16:30:18 +0900

    Init: create 'AppBar'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import AppBar from './components/AppBar';
 import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
+      <AppBar />
       <Home />
     </div>
   );
