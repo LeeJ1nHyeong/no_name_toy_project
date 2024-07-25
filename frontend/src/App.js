80ad6ced1ddd02a97c0ff8a333fe7aa47f05d1e3
LeeJ1nHyeong <wlsgud4084@naver.com>
2024-07-25 13:56:11 +0900

    Feat: Apply react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
-import AppBar from './components/AppBar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AppBar from './components/appbar/AppBar';
 import HomePage from './pages/HomePage';
+import StrategyPage from './pages/StrategyPage';
+import CharacterInfoSearchPage from './pages/CharacterInfoSearchPage';
 
 function App() {
   return (
     <div className="App">
-      <AppBar />
-      <HomePage />
+      <BrowserRouter>
+        <AppBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/strategy" element={<StrategyPage />} />
+          <Route path="/character-info-search" element={<CharacterInfoSearchPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/components/appbar/AppBar.jsx
+++ frontend/src/components/appbar/AppBar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function AppBar() {
   return (
-    <div>AppBar</div>
+    <div>
+      AppBar
+      <div>
+        <Link to="/">홈</Link>
+        <Link to="/strategy">공략</Link>
+        <Link to="/character-info-search">캐릭터 정보 검색</Link>
+      </div>
+    </div>
   )
 }
 
